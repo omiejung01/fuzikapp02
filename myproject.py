cd3a4dcc708d4bf089162803ac70ee914adfd0a3
omiejung01 <36691357+omiejung01@users.noreply.github.com>
2022-06-13 10:35:24 +0700

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello Nuclear Geeks"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
